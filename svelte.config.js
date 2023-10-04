a329c87344bcbba775f2652088b0eeef1a13c7c0
Martin Edström <meedstrom91@gmail.com>
2023-10-04 03:36:10 +0200

    Prerender all public pages

--- svelte.config.js
+++ svelte.config.js
@@ -12,8 +12,13 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
-	}
+		adapter: adapter(),
+        prerender: {
+            handleHttpError: 'warn',
+            handleMissingId: 'warn',
+        },
+	},
+
 };
 
 export default config;
