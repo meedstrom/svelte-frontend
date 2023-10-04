c5b540f763224cee70452719ba3dc789f0ed26f8
Martin Edström <meedstrom91@gmail.com>
2023-10-04 04:04:41 +0200

    Remove now-redundant redirects

--- src/routes/nexus/+page.ts
+++
@@ -1,7 +0,0 @@
-export const prerender = true
-import { redirect } from '@sveltejs/kit'
-
-/** @type {import('./$types').LayoutServerLoad} */
-export function load() {
-    throw redirect(307, '/tq5Q8GX/central-nexus')
-}
