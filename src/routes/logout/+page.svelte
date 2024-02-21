79f44de3e32f4a996ee220398fa2c7323f86a240
Martin Edström <meedstrom91@gmail.com>
2024-02-21 12:48:40 +0100

    Add /logout

---
+++ src/routes/logout/+page.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+ import { get as stored } from 'svelte/store'
+ import {
+     privMeta,
+     pubMeta,
+     allowedTags,
+     bigIndexRows,
+     storedPostKey,
+ } from '$lib/stores'
+ import { privPosts } from '$lib/postContents'
+ import privMetaJSON from '$lib/privMeta.json'
+
+ $allowedTags = []
+ $storedPostKey = ""
+ $privPosts = new Map()
+ $privMeta = new Map()
+
+ $bigIndexRows = [...stored(pubMeta).values()]
+     .filter((post) => !post.tags.includes('stub'))
+     .filter((post) => !post.tags.includes('tag'))
+     .sort((a, b) => b.created.localeCompare(a.created))
+</script>
+
+<h1>Logged out</h1>
+<style>
+ h1 {
+     text-align: center;
+     padding: 3em;
+ }
+</style>
